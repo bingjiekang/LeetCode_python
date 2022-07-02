b6563d5241ab56cc6c0de383b813ffe09bc024df
zhuoyue2 <942844582@qq.com>
2022-07-02 18:36:52 +0800

    更改了顺序和可直接执行

--- 1_两数相加.py
+++ 1_两数相加.py
@@ -1,6 +1,6 @@
 #----1-----3340 ms	15.4 MB
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, target ,*nums):
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 if(nums[i]+nums[j]==target):
@@ -8,11 +8,23 @@ class Solution:
 
    
 #----2-----788 ms	15.4 MB
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums)):
-            temp = target-nums[i]
-            if temp in nums and nums.index(temp)!=i:
-                return [i,nums.index(temp)]
-                
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(0,len(nums)):
+#             temp = target-nums[i]
+#             if temp in nums and nums.index(temp)!=i:
+#                 return [i,nums.index(temp)]
+#
+
+
+def main():
+    sm = Solution()
+    print(sm.twoSum(7,1,2,3,3,5,6))
+
+
+
+
+
+if __name__=="__main__":
+    main()
                 
